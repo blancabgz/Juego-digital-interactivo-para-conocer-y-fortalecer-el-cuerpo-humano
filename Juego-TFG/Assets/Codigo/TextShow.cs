18242cbb2f2f08165eae2765b25ee571f33a316d
Blanca Abril Gonzalez <blancabrilgonz@correo.ugr.es>
2023-11-15 13:27:42 +0100

    cinematica (codigo) terminado y arreglado errores de la cinematica historia

--- Juego-TFG/Assets/Codigo/TextShow.cs
+++ Juego-TFG/Assets/Codigo/TextShow.cs
@@ -11,21 +11,26 @@ public class TextShow : MonoBehaviour
     public Mensaje[] mensajes;
     // array con los actores que participan en la escena
     public Actor[] actores;
-    // array con los actores que estan en el panel del fondo
+    // array con los actores de la escena (niño/a)
     public Actor[] actorPanel;
-    // array con los actores que estan en el panel del fondo
+    // array con los superheroes de la escena
     public Actor[] superheroePanel;
+    // array con las diapositivas
+    public Diapositiva[] imagenes;
     // guardar si la historia ya se ha reproducido
     public string historiaVisionada = "null";
-    // nombre de la escena activa
+    // nombre de la escena a la que vamos a mandar cuando termine la conversacion
+    public string escena;
     
  
 
     public Image avatar;
     public Image avatarInicio;
     public Image avatarSuperheroe;
+    public Image diapositiva;
     public Text conversacion;
     string selectedCharacter;
+    
 
     int mensajeActivo = 0;
 
@@ -36,13 +41,16 @@ public class TextShow : MonoBehaviour
 
     private void Awake(){
         string nombreEscena = SceneManager.GetActiveScene().name;
+        // obtengo el personaje que ha elegido el jugador/a
         selectedCharacter = PlayerPrefs.GetString("SelectedCharacter");
+        // comprueba si la escena ya ha sido visionada en la escena "Historia"
         if(nombreEscena == "Historia"){
             LoadHistoriaVisionada();
         }
         
     }
 
+    // Funcion para que aparezca el sprite del personaje seleccionado por el jugador/a
     public void PersonajePanel(){
         if(selectedCharacter == "boy"){
             Debug.Log(selectedCharacter);
@@ -52,6 +60,8 @@ public class TextShow : MonoBehaviour
         }
     }
 
+    // Funcion para controlar si la historia ha sido visionada, en el caso de que no, la muestra
+    // En caso contrario, no la muestra y va directamente a la escena "Juego"
     public void LoadHistoriaVisionada(){
         if (PlayerPrefs.HasKey("HistoriaVisionada")){
             historiaVisionada = PlayerPrefs.GetString("HistoriaVisionada");
@@ -63,13 +73,13 @@ public class TextShow : MonoBehaviour
         }
     }
 
+    // Funcion que despliega el mensaje junto con el avatar del personaje que habla
     public void DesplegarMensaje(){
         Mensaje mensaje = mensajes[mensajeActivo];
         conversacion.text = mensaje.text;
         Actor actor;
         int id = 0;
-        
-
+    
         //si el personaje seleccionado por el jugador es un chico y contiene el nombre del id la palabra niña
         if(selectedCharacter == "boy" && actores[mensaje.actorId].name.Contains("niña")){
 
@@ -87,25 +97,32 @@ public class TextShow : MonoBehaviour
             actor = actores[mensaje.actorId];
         }
         
+        // avatar del actor que dice el mensaje
         avatar.sprite = actor.sprite;
+        // avatar del superheroe de la escena
         avatarSuperheroe.sprite = superheroePanel[mensaje.id_superheroe].sprite;
+        // imagen de la escena
+        diapositiva.sprite = imagenes[mensaje.id_imagen].sprite;
     }
 
     public void SiguienteMensaje(){
         mensajeActivo++;
         string nombreEscena = SceneManager.GetActiveScene().name;
         
+        // si aun hay mensajes por mostrar, continuo
         if(mensajeActivo < mensajes.Length){
             DesplegarMensaje();
         }else{
+            // si el nombre de la escena es historia, ya se ha visionado, asi que la pongo a true
             if(nombreEscena == "Historia"){
                 historiaVisionada = "true";
+                PlayerPrefs.SetString("HistoriaVisionada", historiaVisionada);
             }
-            PlayerPrefs.SetString("HistoriaVisionada", historiaVisionada);
+            
             if(nombreEscena == "Historia"){
                 SceneManager.LoadScene("Juego"); // going to menu
             }else{
-                //SceneManager.LoadScene();
+                SceneManager.LoadScene(escena);
             }
         }
     }
@@ -116,11 +133,20 @@ public class TextShow : MonoBehaviour
 public class Mensaje {
     public int actorId;
     public int id_superheroe;
+    public int id_imagen;
     public string text;
+    
 }
 
 [System.Serializable] //mostrar los actores en los ajustes
 public class Actor {
+    public int id;
+    public string name;
+    public Sprite sprite;
+}
+
+[System.Serializable] //mostrar las diapositivas
+public class Diapositiva {
     public int id;
     public string name;
     public Sprite sprite;
